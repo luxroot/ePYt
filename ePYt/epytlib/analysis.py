a78a474a7a3fb21842d1bd92acf19583ec73f168
Sihoon Lee <si.lee@kaist.ac.kr>
2021-06-05 23:36:26 +0900

    Merge main branch

--- ePYt/epytlib/analysis.py
+++ ePYt/epytlib/analysis.py
@@ -13,6 +13,5 @@ class Analyzer:
     prim_types = [*map(domain.PrimitiveType, domain.PrimitiveType.prim_types)]
 
     def __init__(self, file_path):
-        script = open(file_path).read()
-        self.user_types = preanalysis.get_typedefs(script)
+        self.user_types = preanalysis.get_typedefs(file_path)
 
