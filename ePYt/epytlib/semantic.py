dac44c7341eb0ba7addfe87f6a62fd9b962ea08a
yongwoo36 <ywlee97@kaist.ac.kr>
2021-06-06 10:23:57 -0700

    Add details

--- ePYt/epytlib/semantic.py
+++ ePYt/epytlib/semantic.py
@@ -1,4 +1,5 @@
 import ast
+import inspect
 from inspect import signature
 from . import graph
 from . import memory
@@ -13,7 +14,12 @@ class Item():
 
 class Semantic(ast.NodeVisitor):
     transfer_type = (graph.Atomic, graph.Branch)
-    op_to_method = {ast.Add: '__add__',
+    op_to_method = {
+        ast.UAdd: '__pos__',
+        ast.USub: '__neg__',
+        ast.Invert: '__invert__',
+        ast.Not: '__not__',
+        ast.Add: '__add__',
         ast.Sub: '__sub__',
         ast.Mult: '__mul__',
         ast.Div: '__div__',
@@ -32,10 +38,22 @@ class Semantic(ast.NodeVisitor):
         ast.GtE: '__ge__',
         ast.In: '__contains__',
         ast.NotIn: '__contains__'}
-    fun_to_method = {'len': '__len__',
-        'str': '__str__',
+    func_to_method = {
+        'abs': '__abs__',
+        'len': '__len__',
         'int': '__int__',
-        'float': '__float__'}
+        'float': '__float__',
+        'complex': '__complex__',
+        'oct': '__oct__',
+        'hex': '__hex__',
+        'bool': '__nonzero__',
+        'str': '__str__',
+        'repr': '__repr__',
+        'unicode': '__unicode__',
+        'string.format': '__format__',
+        'hash': '__hash__',
+        'dir': '__dir__',
+        'size': '__sizeof__'}
 
     def __init__(self, typeinfer: type_infer.TypeInfer):
         self.fixed = False
@@ -48,11 +66,15 @@ class Semantic(ast.NodeVisitor):
     def get_annotation_info(self, args):
         for arg in args:
             self.args.append(arg.arg)
-            annotation = self.get_type(arg.lineno, arg.col_offset)
+            annotation = self.typeinfer.get_type(arg.lineno, arg.col_offset)
             if annotation:
                 self.initial_mem = self.initial_mem.add(arg.arg, annotation)
 
     def lift(self, node):
+        if len(node.instr_list) == 2:
+            if isinstance(node.instr_list[1], ast.Name) and\
+                 ast.unparse(node.instr_list[1]) in self.args:
+                self.items.append(ast.unparse(node.instr_list[1]), '__iter__')
         for instr in node.instr_list:
             if isinstance(instr, self.transfer_type):
                 self.visit(instr)
@@ -77,6 +99,15 @@ class Semantic(ast.NodeVisitor):
                     input_mem = input_mem.join(prev.memory)
                 self.transfer_node(node, input_mem)
     
+    def visit_UnaryOp(self, node):
+        var = ast.unparse(node.operand)
+        if var in self.args:
+            val = domain.HasAttr()
+            val.methods.append(self.op_to_method(type(node.op)))
+            self.items.append(Item(var, val))
+        else:
+            self.generic_visit(node)
+
     def visit_BinOp(self, node):
         var = ast.unparse(node.left)
         if var in self.args:
@@ -84,40 +115,61 @@ class Semantic(ast.NodeVisitor):
             val.methods.append(self.op_to_method(type(node.op)))
             self.items.append(Item(var, val))
         else:
-            self.generic_visit()
+            self.generic_visit(node)
 
     def visit_Compare(self, node):
         var = ast.unparse(node.left)
         if var in self.args:
             val = domain.HasAttr()
-            val.methods.append(self.op_to_method(type(node.op)))
+            val.methods.append(self.op_to_method(type(node.ops[0])))
             self.items.append(Item(var, val))
             return node
         else:
-            self.generic_visit()
+            self.generic_visit(node)
 
     def visit_Call(self, node):
         fun_name = ast.unparse(node.func)
         var = ast.unparse(node.func.value)
-        if isinstance(node.func, ast.Attribute) and var in self.args:
+        if fun_name in self.args:
+            val = domain.HasAttr()
+            val.methods.append('__call__')
+            self.items.append(Item(var, val))
+        elif var in self.args and isinstance(node.func, ast.Attribute):
             val = domain.HasAttr()
             val.methods.append(node.func.attr)
             self.items.append(Item(var, val))
-        elif fun_name in self.func_to_method:
+        elif var in self.args and fun_name in self.func_to_method:
             val = domain.HasAttr()
             val.methods.append(self.func_to_method[fun_name])
             self.items.append(Item(fun_name, val))
         else:
-            self.generic_visit()
+            self.generic_visit(node)
+
+    def visit_Subscript(self, node):
+        var = ast.unparse(node.value)
+        if var in self.args:
+            val = domain.HasAttr()
+            val.methods.append('__index__')
+            if not (isinstance(node.slice, ast.Constant) and type(node.slice.value) == int):
+                val.methods.append('__getitem__')
+                val.methods.append('__setitme__')
+            self.items.append(Item(var, val))
+        else:
+            self.generic_visit(node)
 
     def visit_Attribute(self, node):
         var = ast.unparse(node.value)
         if var in self.args:
             val = domain.HasAttr()
             val.properties.append(node.attr)
+            val.properties.append('__getattr__')
+            val.properties.append('__setattr__')
             self.items.append(Item(var, val))
         else:
-            self.generic_visit()
+            self.generic_visit(node)
+
+    
+
 
     # optional type using primitive type
     # def visit_Assign(self, node):
@@ -135,6 +187,6 @@ class Semantic(ast.NodeVisitor):
     #                 val = domain.PrimitiveType(class_type)
     #                 self.items.append(Item(var, val))
     #                 return node
-    #     return self.generic_visit()
+    #     return self.generic_visit(node)
 
 
