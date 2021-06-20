ded22070343091d431f1855bf12ef798b027f65d
Sihoon Lee <si.lee@kaist.ac.kr>
2021-06-20 17:38:13 +0900

    Fix import and dummy class

--- ePYt/epytlib/annotator.py
+++ ePYt/epytlib/annotator.py
@@ -6,9 +6,26 @@ from pathlib import Path
 from . import analysis
 
 
+def create_import(names):
+    names = list(map(lambda x: ast.alias(name=x), names))
+    import_ = ast.Import(names=names)
+    return import_
+
+
+def create_import_from(module, names):
+    names = list(map(lambda x: ast.alias(name=x), names))
+    import_from = ast.ImportFrom(module=module, names=names, level=0)
+    return import_from
+
+
+def is_my_module(file_info, module_name):
+    return module_name.endswith(f".{file_info.path.stem}")
+
+
 class NodeAnnotator(ast.NodeTransformer):
     def __init__(self, analyzed_file: analysis.FileInfo):
         self.analyzed_file = analyzed_file
+        self.imports = {}
 
     @staticmethod
     def create_union(ids: List[str]):
@@ -34,10 +51,20 @@ class NodeAnnotator(ast.NodeTransformer):
             if not inferred_types:
                 continue
             inferred_type_names = list(
-                map(lambda x: x.class_.__name__, inferred_types))
+                map(
+                    lambda x: f"{x.module_name}.{x.class_.__name__}"
+                    if not is_my_module(self.analyzed_file, x.module_name) else
+                    f"{x.class_.__name__}", inferred_types))
             annotation = self.create_union(inferred_type_names)
             if annotation:
                 arg.annotation = annotation
+            for inferred_type in inferred_types:
+                if inferred_type.module_name not in self.imports:
+                    self.imports[inferred_type.module_name] = []
+                import_name = inferred_type.class_.__name__
+                module_imports = self.imports[inferred_type.module_name]
+                if import_name not in module_imports:
+                    module_imports.append(import_name)
         return node
 
     def visit_FunctionDef(self, node: ast.FunctionDef) -> ast.FunctionDef:
@@ -50,15 +77,31 @@ class NodeAnnotator(ast.NodeTransformer):
             self.annotate_FunctionDef(body, node.name)
         return node
 
-    @staticmethod
-    def create_import_from(module, names):
-        names = list(map(lambda x: ast.alias(name=x), names))
-        import_from = ast.ImportFrom(module=module, names=names, level=0)
-        return import_from
+    def visit_Module(self, node):
+        import_union = create_import_from('typing', ['Union'])
+        node.body.insert(0, import_union)
+        self.generic_visit(node)
+        return node
+
+
+class NodeImporter(ast.NodeTransformer):
+    def __init__(self, file_info, imports: dict):
+        self.file_info = file_info
+        self.imports = imports
 
     def visit_Module(self, node):
-        import_union = self.create_import_from('typing', ['Union'])
-        node.body.insert(0, import_union)
+        for module_name, import_names in self.imports.items():
+            if is_my_module(self.file_info,
+                            module_name):  # TODO: Avoid to import itself
+                for import_name in import_names:
+                    dummy_class_def = ast.ClassDef(name=import_name,
+                                                   decorator_list=[],
+                                                   bases=[],
+                                                   keywords=[], body=[ast.Pass()])
+                    node.body.insert(0, dummy_class_def)
+                continue
+            import_ = create_import([module_name])
+            node.body.insert(0, import_)
         self.generic_visit(node)
         return node
 
@@ -69,6 +112,8 @@ class Annotator:
         tree = deepcopy(analyzed_file.tree)
         node_annotator = NodeAnnotator(analyzed_file)
         tree = node_annotator.visit(tree)
+        node_importer = NodeImporter(analyzed_file, node_annotator.imports)
+        tree = node_importer.visit(tree)
         return tree
 
     @staticmethod
@@ -81,10 +126,3 @@ class Annotator:
             annoatated_path = new_dir_path / relative_path
             annotated_code = ast.unparse(Annotator.annotate(analyzed_file))
             annoatated_path.write_text(annotated_code)
-
-
-'''
-/a/b/c/d.py
-/a/b.annotated
-/a
-'''
